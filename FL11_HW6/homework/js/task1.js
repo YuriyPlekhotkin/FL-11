bcfa92e9941dc46f7c6275cd52c24496f392d85a
YuriyPlekhotkin <Yuriy.Plekhotkin@gmail.com>
2019-07-07 23:52:40 +0300

    HW 6

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,15 +1,15 @@
 // Your code goes here
-var a1 = parseInt(prompt("Please enter a number a1", "0"), 10);
-var a2 = parseInt(prompt("Please enter a number a2", "0"), 10);
-var b1 = parseInt(prompt("Please enter a number b1", "0"), 10);
-var b2 = parseInt(prompt("Please enter a number b2", "0"), 10);
-var c1 = parseInt(prompt("Please enter a number c1", "0"), 10);
-var c2 = parseInt(prompt("Please enter a number c2", "0"), 10);
+const a1 = parseInt(prompt('Please enter a number a1', '0'), 10);
+const a2 = parseInt(prompt('Please enter a number a2', '0'), 10);
+const b1 = parseInt(prompt('Please enter a number b1', '0'), 10);
+const b2 = parseInt(prompt('Please enter a number b2', '0'), 10);
+const c1 = parseInt(prompt('Please enter a number c1', '0'), 10);
+const c2 = parseInt(prompt('Please enter a number c2', '0'), 10);
+const divider = 2;
+const x = (a1 + b1) / divider;
+const y = (a2 + b2) / divider;
 
-var x = (a1 + b1) / 2;
-var y = (a2 + b2) / 2;
-
-if (x == c1 && y == c2) {
+if (x === c1 && y === c2) {
     console.log(true);
 } else {
     console.log(false);
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,16 +1,16 @@
 // Your code goes here
-var a = parseInt(prompt("Please enter a number a", "0"), 10);
-var b = parseInt(prompt("Please enter a number b", "0"), 10);
-var c = parseInt(prompt("Please enter a number c", "0"), 10);
+const a = parseInt(prompt('Please enter a number a', '0'), 10);
+const b = parseInt(prompt('Please enter a number b', '0'), 10);
+const c = parseInt(prompt('Please enter a number c', '0'), 10);
 if (a + b > c && a + c > b && b + c > a) {
-    if (a == b && a == c && b == c) {
-        console.log("Equivalent triangle");
-    } else if (a != b != c) {
-        console.log("Normal triangle");
-    } else if (a == b != c || a != b == c) {
-        console.log("Isosceles triangle");
+    if (a === b && a === c && b === c) {
+        console.log('Equivalent triangle');
+    } else if (a === b !== c || a !== b === c) {
+        console.log('Isosceles triangle');
+    } else if (a !== b !== c) {
+        console.log('Normal triangle');
     }
 
 } else {
-    console.log("Triangle doesn’t exist");
+    console.log('Triangle doesn’t exist');
 }
