268f4f990dc05cf8fa3efff367e1dac00e99abef
YuriyPlekhotkin <Yuriy.Plekhotkin@gmail.com>
2019-07-15 00:19:38 +0300

    HW8

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,6 +1,6 @@
 function formatTime(minutes) {
     return Math.floor(minutes / 1440) +
-        "day(s)" + Math.floor(minutes / 60) +
-        "hour(s)" + minutes % 60 + "minute(s).";
+        ' day(s) ' + Math.floor(Math.abs(minutes / 1440 - Math.floor(minutes / 1440)) * 24) + ' hour(s) ' +
+        minutes % 60 + ' minute(s).';
 }
 formatTime();
