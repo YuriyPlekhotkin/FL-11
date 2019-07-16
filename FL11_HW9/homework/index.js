dd1024d1ce95b1e915d623e6dd4bcfc1b194dc0a
YuriyPlekhotkin <Yuriy.Plekhotkin@gmail.com>
2019-07-16 23:29:36 +0300

    HW 9

---
+++ FL11_HW9/homework/index.js
@@ -0,0 +1,73 @@
+// Your code goes here
+//task 0
+let getNumbers = function(x) {
+    let i, z = [];
+
+    for (i = 0; i < x.length; i++) {
+        let character = x.charAt(i);
+        if (parseInt(character)) {
+            z.push(character);
+        }
+    }
+
+    return z;
+}
+getNumbers('n1um3ber95');
+
+/*
+alternative code
+let getNumbers = function(x) {
+    let i, y, z, arrayLength;
+    y = [];
+    z = [];
+    y = x.split("");
+    arrayLength = y.length;
+
+    for (i = 0; i < arrayLength; i++) {
+        if (parseInt(y[i])) {
+            z.push(y[i]);
+        }
+    }
+
+    return z;
+}
+getNumbers('n1um3ber95'); */
+
+//task2
+
+
+
+let finalList;
+let a, b, c, d, e, f, g;
+let findTypes = function(...types) {
+
+    for (let i = 0; i < types.length; i++) {
+
+        if (typeof types[i] === 'number') {
+            a = '(' + '\'number\'' + ':' + 1 + ')';
+        } else if (typeof types[i] === 'string') {
+
+            b = '(' + '\'string\'' + ':' + 1 + ')';
+        } else if (typeof types[i] === 'boolean') {
+
+            c = '(' + '\'boolean\'' + ':' + 1 + ')';
+        } else if (typeof types[i] === 'symbol') {
+
+            d = '(' + '\'symbol\'' + ':' + 1 + ')';
+        } else if (typeof types[i] === 'undefined') {
+
+            e = '(' + '\'undefined\'' + ':' + 1 + ')';
+        } else if (typeof types[i] === 'object') {
+
+            f = '(' + '\'object\'' + ':' + 1 + ')';
+        } else if (typeof types[i] === 'function') {
+
+            g = '(' + '\'symbol\'' + ':' + 1 + ')';
+
+        }
+    }
+    finalList = a + b + c + d + e + f + g;
+    return finalList;
+}
+let five = 5;
+findTypes(null, five, 'hello');
